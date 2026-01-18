77d11caa20f1a5567bccd336fde14cdb66e8ea9a
cynlopez53 <btcam2019@gmail.com>
2026-01-17 22:11:43 -0600

    Update all page nav links to correct paths, add SVG images for shop tokens, and make pages usable without placeholders

--- assets/js/tribute_tokens.js
+++ assets/js/tribute_tokens.js
@@ -3,25 +3,25 @@ const tributeTokens = [
     id: "token_white_marble",
     name: "White Marble",
     price: 5,
-    img: "assets/js/token_white_marble.png"
+    img: "assets/images/token_white_marble.svg"
   },
   {
     id: "token_black_marble",
     name: "Black Marble",
     price: 5,
-    img: "assets/js/token_black_marble.png"
+    img: "assets/images/token_black_marble.svg"
   },
   {
     id: "token_brown_marble",
     name: "Brown Marble",
     price: 5,
-    img: "assets/js/token_brown_marble.png"
+    img: "assets/images/token_brown_marble.svg"
   },
   {
     id: "token_stone_heart_forever",
     name: "Stone Heart – Forever",
     price: 7,
-    img: "assets/js/token_stone_heart_forever.png"
+    img: "assets/images/token_stone_heart_forever.svg"
   }
 ];
 
